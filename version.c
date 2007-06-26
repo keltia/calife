99d09a15406fe5d3acfc85b05164ab7755b2c200
Ollivier Robert <roberto@keltia.freenix.fr>
2007-06-26 10:37:55 +0200

    Tag 3.0pre7 & update version.c.

--- version.c
+++ version.c
@@ -1,2 +1,2 @@
 
-const char * __version="$Id: @(#)calife revision: 161aef80731e057ce38c0a163aae7e2b19fab581 $";
+const char * __version="$Id: @(#)calife revision: 7e4d3a57409aa8f7611ff7327f08bec0a3c1105d $";
