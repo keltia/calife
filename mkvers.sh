46a6662ea6bdc64b5103890fa2be9f3859fec338
Ollivier Robert <roberto@keltia.freenix.fr>
2009-12-01 16:36:26 +0100

    These two have been unused for quite some time now.

--- mkvers.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/sh
-hg tip --template='\nconst char * __version="$Id: @(#)calife revision: {node|short} $";\n'>version.c
--- version.c
+++
@@ -1,2 +0,0 @@
-
-const char * __version="$Id: version.c,v a0c0e0b4e1c8 2007/10/22 14:42:33 roberto $";
